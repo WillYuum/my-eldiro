8e68abc194f19e906ada2483e905287eefb3371b
WilliamAB <liams_1998@hotmail.com>
2021-11-01 16:36:34 +0200

    Add operator logic

---
+++ src/Utils.rs
@@ -0,0 +1,35 @@
+pub(crate) fn extract_digits(s: &str) -> (&str, &str) {
+    let digits_end = s
+        .char_indices()
+        .find_map(|(idx, c)| if c.is_ascii_digit() { None } else { Some(idx) })
+        .unwrap_or_else(|| s.len());
+
+    let digits = &s[..digits_end];
+    let remainder = &s[digits_end..];
+
+    (remainder, digits)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn extract_one_digit() {
+        assert_eq!(extract_digits("1+2"), ("+2", "1"));
+    }
+
+    fn extract_multiple_digits() {
+        assert_eq!(extract_digits("10-20"), ("-20", "10"));
+    }
+
+    #[test]
+    fn do_not_extract_anything_from_empty_input() {
+        assert_eq!(extract_digits(""), ("", ""));
+    }
+
+    #[test]
+    fn extract_digits_with_no_remainder() {
+        assert_eq!(extract_digits("100"), ("", "100"));
+    }
+}
